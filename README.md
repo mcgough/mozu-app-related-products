d46d7fc5f6746354b1296b6421be7ba5bebd45a3
unknown <dmcgough@dmcgough.flyfish.wan>
2016-03-25 16:53:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# related_products
+### version 0.1.0
+
---
+++ assets/src/storefront.manifest.js
@@ -0,0 +1,7 @@
+module.exports = {
+  
+  'http.storefront.pages.productDetails.request.before': {
+      actionName: 'http.storefront.pages.productDetails.request.before',
+      customFunction: require('./domains/storefront/http.storefront.pages.productDetails.request.before')
+  }
+};
